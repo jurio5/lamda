89c1eac7db56afccc7a85240717d7b036c072586
jurio5 <sia5492@naver.com>
2025-05-05 00:26:25 +0900

    [feat] 동작 매개변수화를 통하여 코드 조각을 유동적으로 처리

--- src/lamda/start/Ex1Main.java
+++ src/lamda/start/Ex1Main.java
@@ -8,11 +8,28 @@ public class Ex1Main {
         long startNs = System.nanoTime();
 
         // 코드 조각 시작
-        int randomValue = new Random().nextInt(6) + 1; // 아마, 이러면 최소 값은 0이 아닌 1로 시작할 것임
+        int randomValue = new Random().nextInt(6) + 1;
         System.out.println("주사위 = " + randomValue);
         // 코드 조각 종료
 
         long endNs = System.nanoTime();
+        System.out.println("실행 시간: " + (endNs + startNs) + "ns");
+    }
+
+    public static void helloSum() {
+        long startNs = System.nanoTime();
+
+        // 코드 조각 시작
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("i = " + i);
+        }
+        // 코드 조각 종료
+        long endNs = System.nanoTime();
         System.out.println("실해 시간: " + (endNs + startNs) + "ns");
+    }
+
+    public static void main(String[] args) {
+        helloDice();
+        helloSum();
     }
 }
