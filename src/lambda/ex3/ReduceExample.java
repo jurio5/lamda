3bb7be7eca5a09be616af320b6f8fc7ad88dd424
jurio5 <sia5492@naver.com>
2025-05-07 02:36:58 +0900

    [feat] 자바에서 지원하는 함수형 인터페이스를 사용하여 문제 풀이

---
+++ src/lambda/ex3/ReduceExample.java
@@ -0,0 +1,24 @@
+package lambda.ex3;
+
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class ReduceExample {
+    // 함수를 인자로 받아, 리스트 요소를 하나로 축약(reduce)하는 고차 함수
+    public static int reduce(List<Integer> list, int initial, BinaryOperator<Integer> reducer) {
+        int sum = initial;
+        for (Integer i : list) {
+            sum = reducer.apply(sum, i);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4);
+        System.out.println("리스트: " + numbers);
+        // 1. 합 구하기 (초깃값 0, 덧셈 로직)
+        System.out.println("합 : " + reduce(numbers, 0, (a, b) -> a + b));
+        // 2. 곱 구하기 (초깃값 1, 곱셈 로직)
+        System.out.println("곱 : " + reduce(numbers, 1, (a, b) -> a * b));
+    }
+}
