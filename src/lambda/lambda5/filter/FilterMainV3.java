be0543c06245a3b3e58c39d884ad7b05a7a42f77
jurio5 <sia5492@naver.com>
2025-05-07 22:00:05 +0900

    [feat] 점진적인 필터 직접 만들기 chapter 1

---
+++ src/lambda/lambda5/filter/FilterMainV3.java
@@ -0,0 +1,27 @@
+package lambda.lambda5.filter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class FilterMainV3 {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        // 짝수만 거르기
+        System.out.println("짝수 = " + filter(numbers, x -> x % 2 == 0));
+        // 홀수만 거르기
+        System.out.println("홀수 = " + filter(numbers, x -> x % 2 != 0));
+
+    }
+
+    private static List<Integer> filter(List<Integer> numbers, Predicate<Integer> predicate) {
+        ArrayList<Integer> filtered = new ArrayList<>();
+        for (Integer number : numbers) {
+            if (predicate.test(number)) {
+                filtered.add(number);
+            }
+        }
+        return filtered;
+    }
+}
