36e525559c32ce463b78ba1dd73e815e2f367225
jurio5 <sia5492@naver.com>
2025-05-07 23:42:28 +0900

    [feat] Stream Method Chain 방식 추가

--- src/lambda/lambda5/mystream/MyStreamV1Main.java
+++ src/lambda/lambda5/mystream/MyStreamV1Main.java
@@ -8,6 +8,7 @@ public class MyStreamV1Main {
         // 짝수만 남기고, 남은 값의 2배를 반환
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         returnValue(numbers);
+        methodChain(numbers);
     }
 
     private static void returnValue(List<Integer> numbers) {
@@ -15,5 +16,13 @@ public class MyStreamV1Main {
         MyStreamV1 filteredStream = stream.filter(n -> n % 2 == 0);
         MyStreamV1 mappedStream = filteredStream.map(n -> n * 2);
         System.out.println("result = " + mappedStream.toList());
+    }
+
+    private static void methodChain(List<Integer> numbers) {
+        List<Integer> result = new MyStreamV1(numbers)
+                .filter(n -> n % 2 == 0)
+                .map(n -> n * 2)
+                .toList();
+        System.out.println("result = " + result);
     }
 }
