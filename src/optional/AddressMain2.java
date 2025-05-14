12c952ae6c83b64715a91853d875ecc4c2c9926d
jurio5 <sia5492@naver.com>
2025-05-15 00:38:11 +0900

    일반 자바 Null 체크 vs Optional 체크 비교

---
+++ src/optional/AddressMain2.java
@@ -0,0 +1,32 @@
+package optional;
+
+import optional.model.Address;
+import optional.model.User;
+
+import java.util.Optional;
+
+public class AddressMain2 {
+
+    public static void main(String[] args) {
+        User user1 = new User("user1", null);
+        User user2 = new User("user2", new Address("hello Street"));
+
+        printStreet(user1);
+        printStreet(user2);
+
+    }
+
+    private static void printStreet(User user) {
+        getUserStreet(user).ifPresentOrElse(
+                System.out::println, // 값이 있을 때
+                () -> System.out.println("Unknown") // 값이 없을 때
+        );
+    }
+
+    static Optional<String> getUserStreet(User user) {
+        return Optional.ofNullable(user) // user 가 null 일 수 있으므로 ofNullable 사용
+                .map(User::getAddress) // user.getAddress() 처리
+                .map(Address::getStreet); // address.getStreet() 처리
+        // 여기서 map 체이닝 중간에 null 이면 Optional.empty()를 반환
+    }
+}
