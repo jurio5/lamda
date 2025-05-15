68274c4baa8f070922fb3c841758ced3bd6b9373
jurio5 <sia5492@naver.com>
2025-05-15 23:54:52 +0900

    [feat] 임계값을 바꿔서 Task 수를 처리하는 쓰레드를 확장

--- src/parallel/forkjoin/SumTask.java
+++ src/parallel/forkjoin/SumTask.java
@@ -9,7 +9,8 @@ import static util.MyLogger.log;
 
 public class SumTask extends RecursiveTask<Integer> {
 
-    private static final int THRESHOLD = 4; // 임계값
+//    private static final int THRESHOLD = 4; // 임계값
+    private static final int THRESHOLD = 2; // 임계값 변경
 
     private final List<Integer> list;
 
